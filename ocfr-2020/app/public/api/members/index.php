07e88068940d9e067396592de3973445f6d681eb
prestonflamion <prestonflamion@gmail.com>
2020-10-16 18:17:41 -0400

    Fixing index.php

--- ocfr-2020/app/public/api/members/index.php
+++ ocfr-2020/app/public/api/members/index.php
@@ -6,22 +6,17 @@ require 'common.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM PatientVisit pv, Patient p WHERE pv.patientGuid = p.patientGuid';
+$sql = 'SELECT * FROM person';
 $vars = [];
 
-// if (isset($_GET['guid'])) {
-//   // This is an example of a parameterized query
-//   $sql = 'SELECT * FROM Patient WHERE patientGuid = ?';
-//   $vars = [ $_GET['guid'] ];
-// }
 
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$patients = $stmt->fetchAll();
+$users = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($patients, JSON_PRETTY_PRINT);
+$json = json_encode($users, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
