3baed965d3b10ba30b5bf914a332d11f07f03b9e
prestonflamion <prestonflamion@gmail.com>
2020-10-16 15:11:23 -0400

    Added back index.app.js

---
+++ ocfr-2020/app/public/js/members0.app.js
@@ -0,0 +1,86 @@
+waitingApp = new Vue({
+  el: '#cardPaneLeft',
+  data:{
+    ptList: [{
+      fname: '',
+      lname: '',
+      address: '',
+      mobilePhone: '',
+      workPhone: '',
+      email: '',
+      dob: '',
+      startDate: '',
+      gender: '',
+      position: '',
+      radioNum: '',
+      stationNum: '',
+      active: '',
+      certifications: ''
+    }],
+      newPTForm: {
+        fname: '',
+        lname: '',
+        address: '',
+        mobilePhone: '',
+        workPhone: '',
+        email: '',
+        dob: '',
+        startDate: '',
+        gender: '',
+        position: '',
+        radioNum: '',
+        stationNum: '',
+        active: '',
+        certifications: ''
+      }
+  },
+  methods:{
+    fetchUser(){
+      fetch('api/members/')
+      .then(response => response.json())
+      .then(json => {
+        this.ptList=json;
+        console.log(this.ptList);
+      });
+    },
+    createUser(){
+      this.newPTForm.userID = (this.newPTForm.fname.substring(0,1)+this.newPTForm.lname).toLowerCase();
+      fetch('api/members/post.php', {
+        method:'POST',
+        body: JSON.stringify(this.newUser),
+        headers: {
+          "Content-Type": "applications/json; charset=utf-8"
+        }
+      })
+      .then( response => response.json() )
+      .then( json => {
+        console.log("Returned from post:", json);
+        this.PtList.push(json[0]);
+        this.newPTForm = this.newUserData();
+      });
+      console.log("Creating (POSTing)...I");
+      console.log(this.newPTForm);
+    },
+    newUserData() {
+      return {
+        fname: '',
+        lname: '',
+        address: '',
+        mobilePhone: '',
+        workPhone: '',
+        email: '',
+        dob: '',
+        startDate: '',
+        gender: '',
+        position: '',
+        radioNum: '',
+        stationNum: '',
+        active: '',
+        certifications: ''
+      }
+    }
+  },
+  created(){
+    this.fetchUser();
+  }
+});
