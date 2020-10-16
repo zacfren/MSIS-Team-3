0a8022133ec5d75bddfb9b70b971d9b94d1d8a84
Ben Kelly <benkell@iu.edu>
2020-10-16 19:26:10 -0400

    Update index.php

--- ocfr-2020/app/public/api/members/index.php
+++ ocfr-2020/app/public/api/members/index.php
@@ -6,22 +6,24 @@ require 'common.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM PatientVisit pv, Patient p WHERE pv.patientGuid = p.patientGuid';
+$sql = 'SELECT * FROM person';
 $vars = [];
 
-// if (isset($_GET['guid'])) {
+if (isset($_GET['empID'])) {
 //   // This is an example of a parameterized query
-//   $sql = 'SELECT * FROM Patient WHERE patientGuid = ?';
-//   $vars = [ $_GET['guid'] ];
-// }
+$sql = 'SELECT * FROM person WHERE empID = ?';
+$vars = [ $_GET['empID'] ];
+}
+
+
 
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$patients = $stmt->fetchAll();
+$ptList = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($patients, JSON_PRETTY_PRINT);
+$json = json_encode($ptList, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
