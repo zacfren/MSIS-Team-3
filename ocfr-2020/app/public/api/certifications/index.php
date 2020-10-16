b17486db21f6b07dfdf6dd6f6af7b4db58550e47
Ben Kelly <benkell@iu.edu>
2020-10-16 18:19:58 -0400

    Update index.php

--- ocfr-2020/app/public/api/certifications/index.php
+++ ocfr-2020/app/public/api/certifications/index.php
@@ -6,22 +6,22 @@ require 'common.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM certification c';
+$sql = 'SELECT * FROM certification';
 $vars = [];
 
-// if (isset($_GET['guid'])) {
+// if (isset($_GET['ID'])) {
 //   // This is an example of a parameterized query
-//   $sql = 'SELECT * FROM Patient WHERE patientGuid = ?';
-//   $vars = [ $_GET['guid'] ];
+//   $sql = 'SELECT * FROM certification WHERE certificationID = ?';
+//   $vars = [ $_GET['ID'] ];
 // }
 
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$patients = $stmt->fetchAll();
+$certification = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($patients, JSON_PRETTY_PRINT);
+$json = json_encode($certification, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
