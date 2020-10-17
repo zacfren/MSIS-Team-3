6a5a7b05f3d61194387e9ea0174c91b393afbe50
Ben Kelly <benkell@iu.edu>
2020-10-16 22:39:53 -0400

    Update index.php

--- ocfr-2020/app/public/api/certifications/index.php
+++ ocfr-2020/app/public/api/certifications/index.php
@@ -6,7 +6,7 @@ require 'common.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM certification c';
+$sql = 'SELECT * FROM certification';
 $vars = [];
 
  if (isset($_GET['certificationID'])) {
