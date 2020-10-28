f318fd6866f19ece8db95d22b66caa12324dcfc1
prestonflamion <prestonflamion@gmail.com>
2020-10-27 20:30:07 -0400

    This evening stuff

--- ocfr-2020/app/public/api/members/post.php
+++ ocfr-2020/app/public/api/members/post.php
@@ -3,7 +3,7 @@
 require 'common.php';
 
 // Only need this line if we're creating GUIDs (see comments below)
-// use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\Uuid;
 
 // Step 0: Validate the incoming data
 // This code doesn't do that, but should ...
@@ -11,7 +11,7 @@ require 'common.php';
 
 // As part of this step, create a new GUID to use as primary key (suitable for cross-system use)
 // If we weren't using a GUID, allowing auto_increment to work would be best (don't pass `id` to `INSERT`)
-// $guid = Uuid::uuid4()->toString(); // i.e. 25769c6c-d34d-4bfe-ba98-e0ee856f3e7a
+$guid = Uuid::uuid4()->toString(); // i.e. 25769c6c-d34d-4bfe-ba98-e0ee856f3e7a
 
 // Step 1: Get a datase connection from our helper class
 $db = DbConnection::getConnection();
