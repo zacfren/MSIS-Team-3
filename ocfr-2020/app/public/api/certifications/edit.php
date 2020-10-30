ef7a8a7197eb268c6e9a29e9cf6559bdbf45fe54
Ben Kelly <benkell@iu.edu>
2020-10-30 00:19:58 -0400

    Update edit.php

--- ocfr-2020/app/public/api/certifications/edit.php
+++ ocfr-2020/app/public/api/certifications/edit.php
@@ -12,7 +12,7 @@ $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 // Note the use of parameterized statements to avoid injection
 $stmt = $db->prepare($sql = 'UPDATE certification SET agency = ?, certificationName = ?, expDate = ? WHERE certificationID=?');
-  $stmt->execute([$_POST['agency'], [$_POST['certificationName'], [$_POST['expDate'], [$_POST['certificationID']]);
+  $stmt->execute([$_POST['agency']], [$_POST['certificationName']], [$_POST['expDate']], [$_POST['certificationID']]);
 
 $certification = $stmt->fetchAll();
 $json = json_encode[$certification, JSON_PRETTY_PRINT];
