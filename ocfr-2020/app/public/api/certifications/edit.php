9cc60a80aae12f2bae10ff4a984522fd521410a6
Ben Kelly <benkell@iu.edu>
2020-10-30 00:23:50 -0400

    Update edit.php

--- ocfr-2020/app/public/api/certifications/edit.php
+++ ocfr-2020/app/public/api/certifications/edit.php
@@ -12,12 +12,10 @@ $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 // Note the use of parameterized statements to avoid injection
 $stmt = $db->prepare($sql = 'UPDATE certification SET agency = ?, certificationName = ?, expDate = ? WHERE certificationID=?');
-  $stmt->execute([$_POST['agency']], [$_POST['certificationName']], [$_POST['expDate']], [$_POST['certificationID']]);
+  $stmt->execute([$_POST['agency'], $_POST['certificationName'], $_POST['expDate'], $_POST['certificationID']]);
 
 $certification = $stmt->fetchAll();
-$json = json_encode[$certification JSON_PRETTY_PRINT];
-
-]);
+$json = json_encode($certification, JSON_PRETTY_PRINT);
 
 // If needed, get auto-generated PK from DB
  //$certificationID = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
