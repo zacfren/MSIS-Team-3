a88003f47d00a617d9f8b1513fd34b9f5abd7bec
Ben Kelly <benkell@iu.edu>
2020-10-30 00:30:47 -0400

    Update delete.php

--- ocfr-2020/app/public/api/certifications/delete.php
+++ ocfr-2020/app/public/api/certifications/delete.php
@@ -24,7 +24,7 @@ $delcert = $stmt->fetchAll();
 $json = json_encode($delcert, JSON_PRETTY_PRINT);
 
 // If needed, get auto-generated PK from DB
- //$certificationID = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
+ $certificationID = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
 
 // Step 4: Output
 // Here, instead of giving output, I'm redirecting to the SELECT API,
