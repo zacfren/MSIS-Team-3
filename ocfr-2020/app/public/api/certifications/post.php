3f4bbf8cd71be730ea501b3c47ea3ccacded286e
Ben Kelly <benkell@iu.edu>
2020-10-30 00:36:05 -0400

    Update post.php

--- ocfr-2020/app/public/api/certifications/post.php
+++ ocfr-2020/app/public/api/certifications/post.php
@@ -24,7 +24,7 @@ $stmt->execute([
 ]);
 
 // If needed, get auto-generated PK from DB
-// $pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
+ $certificationID = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
 
 // Step 4: Output
 // Here, instead of giving output, I'm redirecting to the SELECT API,
