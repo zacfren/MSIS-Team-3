d82b4453b75e3444a4ac08a0434cc261cdd2aedc
prestonflamion <prestonflamion@gmail.com>
2020-11-01 23:30:59 -0500

    Created reports and reports 2 (.html, .js)

--- ocfr-2020/app/public/js/reports.app.js
+++ ocfr-2020/app/public/js/reports.app.js
@@ -1,22 +1,56 @@
-var memberReport = new Vue({
+var reportsapp = new Vue({
   el: '#memberReport',
   data: {
-    reportsList: [],
-    activeReport: null,
-    },
+    reportsList:[{
+      reportsID:'',
+      certificationID:'',
+      empID:'',
+      fname:'',
+      lname:'',
+      expDate:'',
+      certificationName:''
+    }],
+  },
 
-    methods:{
-      fetchUser(){
-        fetch('api/reports/')
-        .then(response => response.json())
-        .then(json => {
-          this.reportsList=json;
-          console.log(this.reportsList);
-        });
+  methods: {
+    fetchReports(){
+      fetch('api/reports/index.php')
+      .then( response => response.json() )
+      .then( json => {
+        this.reportsList = json;
+        console.log(this.reportsList)});
       }
-    },
-
-  created(){
-    this.fetchUser();
-    this.fetchCert();  }
+},
+ created() {
+  this.fetchReports();
+ }
 });
+
+
+
+
+
+
+
+// var memberReport = new Vue({
+//   el: '#memberReport',
+//   data: {
+//     reportsList: [],
+//     computed:
+//     },
+//
+//     methods:{
+//       fetchUser(){
+//         fetch('api/reports/')
+//         .then(response => response.json())
+//         .then(json => {
+//           this.reportsList=json;
+//           console.log(this.reportsList);
+//         });
+//       }
+//     },
+//
+//   created(){
+//     this.fetchUser();
+//     this.fetchCert();  }
+// });
---
+++ ocfr-2020/app/public/js/reports2.app.js
@@ -0,0 +1,42 @@
+var reports2app = new Vue({
+  el: '#memberReport2',
+  data: {
+    reports2List:[{
+      reportsID:'',
+      certificationID:'',
+      empID:'',
+      fname:'',
+      lname:'',
+      expDate:'',
+      certificationName:'',
+      stationNum:'',
+      radioNum:''
+    }],
+    computed: {
+  sortedArray: function() {
+    function compare(a, b) {
+      if (a.stationNum < b.stationNum)
+        return -1;
+      if (a.stationNum > b.stationNum)
+        return 1;
+      return 0;
+    }
+
+    return this.arrays.sort(compare);
+  }
+}
+  },
+
+  methods: {
+    fetchReports(){
+      fetch('api/reports/index.php')
+      .then( response => response.json() )
+      .then( json => {
+        this.reportsList = json;
+        console.log(this.reportsList)});
+      }
+},
+ created() {
+  this.fetchReports();
+ }
+});
