b912593a81a742473dcf446f99f8fe1496042f78
Ben Kelly <benkell@iu.edu>
2020-11-02 21:50:22 -0500

    fixing errors
    fixed routing in the reports/post.php, added certFilter in the event we need it for sorting

--- ocfr-2020/app/public/api/reports/post.php
+++ ocfr-2020/app/public/api/reports/post.php
@@ -15,7 +15,7 @@ $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 // Note the use of parameterized statements to avoid injection
 $stmt = $db->prepare(
-  'INSERT INTO person (certificationID, empID, fname, lname, expDate, certificationName)
+  'INSERT INTO reports (certificationID, empID, fname, lname, expDate, certificationName)
   VALUES (?, ?, ?, ?, ?, ?)'
 );
 
@@ -29,10 +29,10 @@ $stmt->execute([
 ]);
 
 // If needed, get auto-generated PK from DB
-// $pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
+ $reportsID = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
 
 // Step 4: Output
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
 header('HTTP/1.1 303 See Other');
-header('Location: ../reports/');
+header('Location: ../reports/' . $reportsID);
